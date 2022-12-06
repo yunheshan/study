fefe91569d9a665dba66630bc93ba45ee2c4a12b
dengqinghong <dengqinghong@baohanghr.com>
2022-12-06 16:45:46 +0800

    测试类模板

---
+++ src/test/java/com/bcs/study/TestWithSpring.java
@@ -0,0 +1,25 @@
+package com.bcs.study;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWithSpring {
+
+    @BeforeAll
+    static void before(){
+
+    }
+
+    @AfterAll
+    static void after(){
+
+    }
+
+    @Test
+    void test() {
+    }
+
+}
---
+++ src/test/java/com/bcs/study/TestWithoutSpring.java
@@ -0,0 +1,17 @@
+package com.bcs.study;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author bcs
+ * @Date 2022/12/6 16:15
+ * @Version 1.0
+ * @Desc
+ */
+public class TestWithoutSpring {
+
+    @Test
+    public void test(){
+        System.out.println(".".split("\\.").length);
+    }
+}
