61e0e5668b61b9334a42e111aad3d251222f3d39
yunheshan <508146801@qq.com>
2022-12-06 11:37:44 +0800

    测试提交

---
+++ src/main/java/com/bcs/study/StudyApplication.java
@@ -0,0 +1,16 @@
+package com.bcs.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dqh
+ */
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+
+}
