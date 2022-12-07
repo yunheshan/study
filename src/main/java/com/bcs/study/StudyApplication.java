39e8041065e96659f0e8e237a6ae0146c78cad02
yunheshan <508146801@qq.com>
2022-12-07 16:07:52 +0800

    测试2

--- src/main/java/com/bcs/study/StudyApplication.java
+++ src/main/java/com/bcs/study/StudyApplication.java
@@ -1,6 +1,5 @@
 package com.bcs.study;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Author bcs
  */
 @SpringBootApplication
-@MapperScan("com.bcs.study.module.*.mapper")
 public class StudyApplication {
 
     public static void main(String[] args) {
